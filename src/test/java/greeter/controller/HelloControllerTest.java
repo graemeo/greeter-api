9b87dd611296842b61580ba82d03be6746b4a966
Graeme O <graeme.ong>
2017-11-03 13:47:36 +0800

    [GraemeO] Add simple controller test

---
+++ src/test/java/greeter/controller/HelloControllerTest.java
@@ -0,0 +1,36 @@
+package greeter.controller;
+
+import static org.hamcrest.Matchers.equalTo;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@AutoConfigureMockMvc
+public class HelloControllerTest {
+ 
+   @Autowired
+   private MockMvc mockMvc;
+  
+   @Test
+   public void shouldReturnSuccessfulResponse() throws Exception {
+      // given
+      // when
+      // then
+      mockMvc.perform(MockMvcRequestBuilders.get("/hello"))
+         .andExpect(status().isOk())
+         .andExpect(content().string(equalTo("Hello there!")));
+   }
+
+}

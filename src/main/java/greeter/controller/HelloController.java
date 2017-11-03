e124616a7ea46698693491eb01d6679e7b6fbfad
Graeme O <graeme.ong>
2017-11-03 13:10:22 +0800

    [GraemeO] Add context path and simple controller

---
+++ src/main/java/greeter/controller/HelloController.java
@@ -0,0 +1,13 @@
+package greeter.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RestController
+public class HelloController {
+   
+   @RequestMapping("/hello")
+   public String hello() {
+      return "Hello there!";
+   }
+}
